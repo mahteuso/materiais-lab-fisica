0afb092706c36c50a0f181c931c4bbf24982aaf1
mahteuso <mateus@ufscar.br>
2024-07-25 08:37:13 -0300

    Config: Staring the project with .gitignore, pom.xml and src/

---
+++ src/main/java/com/equipamentos/lab/fisica/LabFisicaApiApplication.java
@@ -0,0 +1,13 @@
+package com.equipamentos.lab.fisica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabFisicaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabFisicaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equipamentos/lab/fisica/LabFisicaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipamentos.lab.fisica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabFisicaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
